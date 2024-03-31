f54ae3b472ce15c800c131ff224467fbce0bcfd8
Andreas Avetisian <t0avan00@students.oamk.fi>
2024-03-31 20:02:58 +0200

    Finished implementing "Upcoming Events"; Created skeletons for future components; Used query to fetch data from database provided by Refine;

--- src/utilities/date/get-date-colors.ts
+++ src/utilities/date/get-date-colors.ts
@@ -2,20 +2,22 @@ import dayjs from "dayjs";
 
 type DateColors = "success" | "processing" | "error" | "default" | "warning";
 
+//  returns a color based on the date
 export const getDateColor = (args: {
-    date: string;
-    defaultColor?: DateColors;
+  date: string;
+  defaultColor?: DateColors;
 }): DateColors => {
-    const date = dayjs(args.date);
-    const today = dayjs();
+  const date = dayjs(args.date);
+  const today = dayjs();
 
-    if (date.isBefore(today)) {
-        return "error";
-    }
+  if (date.isBefore(today)) {
+    return "error";
+  }
 
-    if (date.isBefore(today.add(3, "day"))) {
-        return "warning";
-    }
+  if (date.isBefore(today.add(3, "day"))) {
+    return "warning";
+  }
 
-    return args.defaultColor ?? "default";
+  // ?? is the nullish coalescing operator. It returns the right-hand side operand when the left-hand side is null or undefined.
+  return args.defaultColor ?? "default";
 };
